4f0f332d8922dc25a9000c45b4067823b74c4be1
G-Shree-Harsha <115868534+G-Shree-Harsha@users.noreply.github.com>
2022-10-15 14:24:11 +0530

    Add files via upload

---
+++ Triangle Third ngle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter Two Angles Of A Triangle: ");
+    scanf("%d%d", &a,&b);
+    c = 180 -(a+b);
+    printf("Third Angle OF The Traiangle = %d, c");
+    return 0;
+
+
+}
